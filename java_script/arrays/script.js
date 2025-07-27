f3a456813b4b3bc9329bca89d4262615b8522c3f
divya <divyaelzjohn@gmail.com>
2025-07-27 17:06:29 +0530

    JS

--- java_script/arrays/script.js
+++ java_script/arrays/script.js
@@ -9,7 +9,7 @@ document.write(animal[0])
 
 // how many element in an array
 
-
+/*
 var animal =["elephant","lion","cat","lion"]
 document.write(animal[0]+"<br>")
 animal[0]="Tiger"
@@ -19,3 +19,19 @@ var animal=['Elephent',"lion","cat","Lion"]
 animal[0]="Tiger"
 document.write(animal[0])
 dsument.write(animal[0]+"br")
+
+var animal=[elelphen
+    document.write(animal[0])
+    anima[0]="lion"
+    
+]*/
+
+
+
+// const numbers=[1,2,3,4,5]
+// const doubled=[]
+
+// for(let i=0;i<numbers.length;i++){
+//     dobled
+// }
+
